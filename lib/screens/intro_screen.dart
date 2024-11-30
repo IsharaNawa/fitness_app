9a787487d9c5fde878b46e99b22e08ee42a41c21
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2024-12-01 02:45:24 +0530

    added screen navigation

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_app/data/intro_screen_models.dart';
 import 'package:fitness_app/models/intro_screen_model.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -12,121 +13,143 @@ class IntroScreen extends StatelessWidget {
     required this.isFadingNeeded,
   });
 
+  void navigateToNextIntroScreen(BuildContext context) {
+    int index = introScreenModels
+        .indexWhere((model) => model.mainTitle == introScreenModel.mainTitle);
+    index = (index + 1) % introScreenModels.length;
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => IntroScreen(
+          introScreenModel: introScreenModels[index],
+          isFadingNeeded: false,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned(
-          top: -50,
-          left: -150,
-          child: Image.asset(
-            introScreenModel.getBackgroundImagePath,
-            width: 850,
-            height: 950,
-            fit: BoxFit.cover,
-          ),
-        ),
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(left: 40, bottom: 50),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      introScreenModel.getmainTitle,
-                      textAlign: TextAlign.start,
-                      style: GoogleFonts.lato(
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      introScreenModel.getDescription,
-                      style: GoogleFonts.poppins(
-                        fontSize: 15,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
+    return Scaffold(
+      body: InkWell(
+        onTap: () {
+          navigateToNextIntroScreen(context);
+        },
+        child: Stack(
+          children: [
+            Positioned(
+              top: -10,
+              left: -110,
+              child: Image.asset(
+                introScreenModel.getBackgroundImagePath,
+                width: 800,
+                height: 1000,
+                fit: BoxFit.cover,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  FilledButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor:
-                          const WidgetStatePropertyAll<Color>(Colors.white),
-                      padding: WidgetStateProperty.all(
-                        const EdgeInsets.symmetric(
-                          vertical: 15,
-                          horizontal: 30,
+                  Container(
+                    padding: const EdgeInsets.only(left: 40, bottom: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          introScreenModel.getmainTitle,
+                          textAlign: TextAlign.start,
+                          style: GoogleFonts.lato(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      shape: WidgetStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
+                        const SizedBox(
+                          height: 20,
                         ),
-                      ),
-                    ),
-                    child: Text(
-                      "LOG IN",
-                      style: GoogleFonts.lato(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w900,
-                        letterSpacing: 7.0,
-                      ),
+                        Text(
+                          introScreenModel.getDescription,
+                          style: GoogleFonts.poppins(
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  OutlinedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      padding: WidgetStateProperty.all(
-                        const EdgeInsets.symmetric(
-                          vertical: 15,
-                          horizontal: 30,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FilledButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor:
+                              const WidgetStatePropertyAll<Color>(Colors.white),
+                          padding: WidgetStateProperty.all(
+                            const EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 30,
+                            ),
+                          ),
+                          shape: WidgetStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          "LOG IN",
+                          style: GoogleFonts.lato(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w900,
+                            letterSpacing: 7.0,
+                          ),
                         ),
                       ),
-                      shape: WidgetStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
+                      OutlinedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          padding: WidgetStateProperty.all(
+                            const EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 30,
+                            ),
+                          ),
+                          shape: WidgetStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          side: WidgetStateProperty.all(
+                            const BorderSide(
+                              color: Colors.white,
+                              width: 2,
+                            ),
+                          ),
                         ),
-                      ),
-                      side: WidgetStateProperty.all(
-                        const BorderSide(
-                          color: Colors.white,
-                          width: 2,
+                        child: Text(
+                          "SIGN UP",
+                          style: GoogleFonts.lato(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 7.0,
+                          ),
                         ),
-                      ),
-                    ),
-                    child: Text(
-                      "SIGN UP",
-                      style: GoogleFonts.lato(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 7.0,
-                      ),
-                    ),
-                  )
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
                 ],
               ),
-              const SizedBox(
-                height: 50,
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,3 +1,5 @@
+import 'package:fitness_app/data/intro_screen_models.dart';
+import 'package:fitness_app/screens/intro_screen.dart';
 import 'package:fitness_app/widgets/start_screen_image_circle_container.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -5,89 +7,105 @@ import 'package:google_fonts/google_fonts.dart';
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
+  void navigateToIntroScreens(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => IntroScreen(
+          introScreenModel: introScreenModels[0],
+          isFadingNeeded: false,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const SizedBox(
-            height: 60,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/app_symbol.png',
-                width: 28,
-              ),
-              const SizedBox(width: 12),
-              Text(
-                "FITNESS",
-                style: GoogleFonts.jetBrainsMono(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 6,
-                ),
-              ),
-            ],
-          ),
-          const StartScreenImageCircleContainer(),
-          Text(
-            "Let's Move",
-            style: GoogleFonts.lato(
-              color: Colors.black,
-              fontSize: 36,
-              fontWeight: FontWeight.w700,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(
+              height: 60,
             ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          SizedBox(
-            width: 250,
-            child: Text(
-              "Fitness and wellness for you anytime, anywhere",
-              style: GoogleFonts.poppins(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          FilledButton(
-            onPressed: () {},
-            style: ButtonStyle(
-              backgroundColor:
-                  const WidgetStatePropertyAll<Color>(Colors.black),
-              padding: WidgetStateProperty.all(
-                const EdgeInsets.symmetric(
-                  vertical: 15,
-                  horizontal: 50,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/app_symbol.png',
+                  width: 28,
                 ),
-              ),
-              shape: WidgetStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+                const SizedBox(width: 12),
+                Text(
+                  "FITNESS",
+                  style: GoogleFonts.jetBrainsMono(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 6,
+                  ),
                 ),
-              ),
+              ],
             ),
-            child: Text(
-              "GET STARTED",
+            const StartScreenImageCircleContainer(),
+            Text(
+              "Let's Move",
               style: GoogleFonts.lato(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 7.0,
+                color: Colors.black,
+                fontSize: 36,
+                fontWeight: FontWeight.w700,
               ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 25,
+            ),
+            SizedBox(
+              width: 250,
+              child: Text(
+                "Fitness and wellness for you anytime, anywhere",
+                style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            FilledButton(
+              onPressed: () {
+                navigateToIntroScreens(context);
+              },
+              style: ButtonStyle(
+                backgroundColor:
+                    const WidgetStatePropertyAll<Color>(Colors.black),
+                padding: WidgetStateProperty.all(
+                  const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 50,
+                  ),
+                ),
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              child: Text(
+                "GET STARTED",
+                style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 7.0,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
